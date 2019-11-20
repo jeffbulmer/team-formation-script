f6df9859e40ee49a0149c24146da2faf4c16e39d
apinchbeck <angie.pinchbeck@gmail.com>
2019-11-20 08:09:19 -0800

    created fastt.py

---
+++ team_formation/fastt.py
@@ -0,0 +1,20 @@
+from SocialNetwork import SocialNet
+
+class FASTT:
+    """
+    An instance of the Fair Allocation of Several Teams to Tasks is 
+    given by:
+
+    N = [n] a set of projects
+    O = {o_1, o_2,...,o_m} a set of employees
+    G(O,E) = a graph representing the underlying social network
+    R = {r_1,...r_n} the requirements for each project 
+    S = {s_1,...,s_m} the skill sets for each employee
+    U = the set of utility functions
+
+    """
+
+    def __init__(self, projects, employees):
+        self.n = projects
+        self.o = employees
+        #self.g = 
